4e9661618cb6ea136dba9d04f36b8deb8da70830
Stelios P. <63586679+Steliospne@users.noreply.github.com>
2023-12-11 15:41:30 +0100

    added logic to dynamicly change the size of the grid and border size.

--- script.js
+++ script.js
@@ -1,21 +1,88 @@
+const cells = +prompt("Enter grid size per side.");
 const primary_container = document.querySelector(".primary-container");
-let row1, row2, row3, row4;
-let col1, col2, col3, col4;
-const rowArray = [row1, row2, row3, row4];
-const colArray = [col1,col2,col3,col4];
-let i = 1;
-let k = 1;
-for (colms in colArray) {
-    colms = document.createElement('div')
-    colms.className = `col${k}`;
-    primary_container.appendChild(colms);
-    k =++k;
-    for (rows of rowArray) {
-        rows = document.createElement('div');
-        rows.className = `row${i}`;
-        colms.appendChild(rows);
-        i = ++i;
+const containerMaxWidth = primary_container.offsetWidth;
+
+createGrid(cells);
+
+function createGrid(cells) {
+    for (let i = 1; i < cells ** 2 + 1; i++) {
+        const gridItem = document.createElement("div");
+        let gridItemTop, gridItemRight, gridItemBottom, gridItemLeft;
+        gridItem.className = `grid-item${i}`;
+        gridItem.textContent = i;
+        gridItem.style.boxSizing = "border-box";
+        gridItem.style.width = containerMaxWidth / cells + "px";
+        gridItem.style.height = gridItem.style.width;
+        gridItem.style.border = "solid black";
+
+        setGridBorders(
+            i,
+            gridItemTop,
+            gridItemRight,
+            gridItemBottom,
+            gridItemLeft,
+            cells,
+            gridItem
+        );
+        primary_container.appendChild(gridItem);
     }
 }
 
-
+function setGridBorders(
+    i,
+    gridItemTop,
+    gridItemRight,
+    gridItemBottom,
+    gridItemLeft,
+    cells,
+    gridItem
+) {
+    if (i == 1) {
+        gridItemTop = 4;
+        gridItemRight = 2;
+        gridItemBottom = 2;
+        gridItemLeft = 4;
+    } else if (i == cells) {
+        gridItemTop = 4;
+        gridItemRight = 4;
+        gridItemBottom = 2;
+        gridItemLeft = 2;
+    } else if (i == 3 * cells + 1) {
+        gridItemTop = 2;
+        gridItemRight = 2;
+        gridItemBottom = 4;
+        gridItemLeft = 4;
+    } else if (i == cells ** 2) {
+        gridItemTop = 2;
+        gridItemRight = 4;
+        gridItemBottom = 4;
+        gridItemLeft = 2;
+    } else if (i > 0 && i < cells) {
+        gridItemTop = 4;
+        gridItemRight = 2;
+        gridItemBottom = 2;
+        gridItemLeft = 2;
+    } else if (i % cells == 0 && i != 0 && i != cells ** 2) {
+        gridItemTop = 2;
+        gridItemRight = 4;
+        gridItemBottom = 2;
+        gridItemLeft = 2;
+    } else if (i % cells == 1 && i != 1 && i != 3* cells + 1) {
+        gridItemTop = 2;
+        gridItemRight = 2;
+        gridItemBottom = 2;
+        gridItemLeft = 4;
+    } else if (i > 3 * cells + 1 && i< cells **2) {
+        gridItemTop = 2;
+        gridItemRight = 2;
+        gridItemBottom = 4;
+        gridItemLeft = 2;
+    }
+    else {
+        gridItemTop = 2;
+        gridItemRight = 2;
+        gridItemBottom = 2;
+        gridItemLeft = 2;
+    }
+    console.log(gridItem.style.borderWidth = new String(`${gridItemTop}px ${gridItemRight}px ${gridItemBottom}px ${gridItemLeft}px`));
+}
